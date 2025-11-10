b767e64c3bcfdd77c2a17bf389a2cd8b4d8fa9cd
Velkorom <velkoromvelkorom@gmail.com>
2025-11-10 20:30:12 +0100

    Homework04

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,24 @@
+import ".styles.css";
+import Button from "../Button/Button";
+import { useState } from "react";
+
+function Feedback (){
+
+    return(
+        <div className="feedback-conteiner">
+            <div className="feedback-result-conteiner">
+                <div className="like-dislike-conteiner">
+                    <div className="result">0</div>
+                    <Button name="Like" onClick={() => {}} />
+                </div>
+                <div className="like-dislike-conteiner">
+                    <div className="result">0</div>
+                    <Button name="DisLike" onClick={() => {}} />
+                </div>
+                </div>
+                <Button name="RESET RESULTS" onClick={() =>{}} />
+            </div>
+    );
+}
+
+export default Feedback;
